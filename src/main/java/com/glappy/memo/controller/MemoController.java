2097eff84458e205a434e1d1fb6afd344af71f3e
glappy <glappy@naver.com>
2019-05-15 22:02:05 +0900

    바뀐것없음

--- src/main/java/com/glappy/memo/controller/MemoController.java
+++ src/main/java/com/glappy/memo/controller/MemoController.java
@@ -1,7 +1,8 @@
 package com.glappy.memo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,14 +19,10 @@ public class MemoController {
 		return "memo_write";
 	}
 	@RequestMapping(value="/memo_write",method=RequestMethod.POST)
-	public String memo_write(MemoVO memoVO) {
-		/* 매개변수에 일일이 long id, String m_auth..와 같이 하는 것은 VO로 묶으면 될 것 같습니다
-		 * jsp에서나 controller에서나 post로 설정을 했는데 메서드 문제가 있다고 405오류가 뜹니다.
-		 * 그게 돼야 log가 제대로 되는지를 보던가 말던가 할 것 같은데..
-		 * 일단 계속 해보겠습니다.*/
+	public String memo_write(@ModelAttribute("memoVO") MemoVO memoVO, Model model) {
 		MemoVO vo=new MemoVO();
 		log.debug(vo.toString());
-		return "redirect:home";
+		return "redirect:/";
 	}
 	@RequestMapping(value="/memo_list", method=RequestMethod.GET)
 	public String memo_list() {
