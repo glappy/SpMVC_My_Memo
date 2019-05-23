f48597859c43c8ddfd233597eb0d3c57d3342ca8
glappy <glappy@naver.com>
2019-05-23 16:45:12 +0900

    mybatis bean에서 class문제

---
+++ src/main/java/com/glappy/memo/mapper/MemoDaoImp.java
@@ -0,0 +1,29 @@
+package com.glappy.memo.mapper;
+
+public class MemoDaoImp implements MemoDao {
+
+	@Override
+	public String selectAll() {
+		return null;
+	}
+
+	@Override
+	public String findById(long id) {
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		return 0;
+	}
+
+	@Override
+	public int insert(long id) {
+		return 0;
+	}
+
+	@Override
+	public int update(long id) {
+		return 0;
+	}
+}
