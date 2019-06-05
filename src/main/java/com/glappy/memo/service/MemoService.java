bae79d5f9b37ad19403a07fad35d8e4635aa74aa
glappy <glappy@naver.com>
2019-06-05 17:07:14 +0900

    service class 수정

--- src/main/java/com/glappy/memo/service/MemoService.java
+++ src/main/java/com/glappy/memo/service/MemoService.java
@@ -4,36 +4,32 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.glappy.memo.mapper.MemoDao;
 import com.glappy.memo.model.MemoVO;
 
-@Repository
-public class MemoService implements MemoDao {
+@Service
+public class MemoService{
 	@Autowired
 	MemoDao memoDao;
 
-	@Override
 	public List<MemoVO> selectAll() {
 		List<MemoVO> memoList=memoDao.selectAll();
 		return memoList;
 	}
-	@Override
 	public MemoVO findById(long id) {
 		MemoVO memoVO=memoDao.findById(id);
 		return memoVO;
 	}
-	@Override
 	public int delete(long id) {
 		int ret=memoDao.delete(id);
 		return ret;
 	}
-	@Override
 	public int insert(MemoVO memoVO) {
 		int ret=memoDao.insert(memoVO);
 		return ret;
 	}
-	@Override
 	public int update(MemoVO memoVO) {
 		int ret=memoDao.update(memoVO);
 		return ret;
